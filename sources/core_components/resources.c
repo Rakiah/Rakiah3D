bb10c1b3b0a2effc9d50de3793941b1e48600f15
Rakiah <rakiah78600@live.fr>
2016-07-27 16:00:36 +0200

    load resource only if asked atleast once

--- sources/core_components/resources.c
+++ sources/core_components/resources.c
@@ -6,7 +6,7 @@
 /*   By: bkabbas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/13 02:33:32 by bkabbas           #+#    #+#             */
-/*   Updated: 2016/07/09 19:10:16 by Rakiah           ###   ########.fr       */
+/*   Updated: 2016/07/27 16:00:09 by Rakiah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void		resources_load(void)
 		if (lines[i][0] != '\0')
 		{
 			split = rstd_strsplit(lines[i], ' ', NULL);
-			resources_add(split[0], load(split[1]));
+			resources_add(split[0], split[1]);
 			rstd_delete_tab((void **)split, 2);
 		}
 		free(lines[i++]);
@@ -35,16 +35,26 @@ void		resources_load(void)
 	free(lines);
 }
 
-void		resources_add(char *name, void *data)
+void		resources_add(char *name, char *path)
 {
-	t_resource *resource;
+	t_resource	*resource;
 
 	resource = malloc(sizeof(t_resource));
 	resource->name = rstd_strdup(name);
-	resource->data = data;
+	resource->path = rstd_strdup(path);
+	resource->data = NULL;
 	list_push_back(get_core()->resources, resource);
-	rstd_putstr("Included resource : ");
-	rstd_putendl(name);
+}
+
+void		*resources_poll(t_resource *res)
+{
+	if (res->data == NULL)
+	{
+		res->data = load(res->path);
+		rstd_putstr("Included resource : ");
+		rstd_putendl(res->name);
+	}
+	return (res->data);
 }
 
 void		*resources_find(char *name)
@@ -56,6 +66,6 @@ void		*resources_find(char *name)
 	list_set_start(resources);
 	while ((it = list_next(resources)) != NULL)
 		if (rstd_strcmp(name, it->name) == 0)
-			return (it->data);
+			return (resources_poll(it));
 	return (NULL);
 }
